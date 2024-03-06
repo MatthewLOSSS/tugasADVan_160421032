192ea160d89053de6e9181777140ff36e9d9a202
matthew <matthew07102003@gmail.com>
2024-03-06 21:28:44 +0700

    hasil

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.ubaya.anmp_week1.R
+import com.ubaya.anmp_week1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.navHost)
+                    as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
+}
+
+
---
+++ app/src/main/java/com/example/myapplication/resultFragment.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ubaya.anmp_week1.databinding.FragmentGameBinding
+import com.ubaya.anmp_week1.databinding.FragmentResultBinding
+
+class resultFragment : Fragment() {
+    private lateinit var binding: FragmentResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentResultBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    //akses tombol
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(arguments != null) {
+            val score =
+                resultFragmentArgs.fromBundle(requireArguments()).score
+            binding.txtScore.text = "Your score is $score"
+        }
+
+
+        binding.btnMainScreen.setOnClickListener{
+            val action =  resultFragmentDirections.actionMainFragmentResult()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
